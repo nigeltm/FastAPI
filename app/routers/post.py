8216e7c1007dd32bc30b948ce9e0aae107996944
Nigel Mafukidze <ntmafukidze@gmail.com>
2021-11-21 18:03:58 +0200

    Add post owner and only allow post owner to perform operations on  a post

--- app/routers/post.py
+++ app/routers/post.py
@@ -31,8 +31,7 @@ def get_posts(db:Session = Depends(get_db),current_user: int = Depends(oauth2.ge
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
 def create_post(post:schemas.PostCreate,db: Session = Depends(get_db),
   current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)
-    post = models.Post(**post.dict())
+    post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(post)
     db.commit()
     # TO RETURN NEWLY CREATED POST IN THE ENDPOINT
@@ -66,10 +65,13 @@ current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" DELETE FROM posts WHERE id= %s RETURNING * """,(str(id),))
     # post=cursor.fetchone()
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id==id)
-    if post.first()==None:
+    post_query = db.query(models.Post).filter(models.Post.id==id)
+    post = post_query.first()
+    if post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {id} does not exist.")
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Not authorized to perform this operation")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -86,6 +88,9 @@ def update_post(id:int,updated_post:schemas.PostCreate,
     post=post_query.first()
     if post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {id} does not exist.")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Not authorized to perform this operation")
+
     post_query.update(updated_post.dict(),synchronize_session=False)
     db.commit()
     return post_query.first()
