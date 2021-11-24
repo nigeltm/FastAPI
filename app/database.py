9864429b92f257518a8c43131965c9a62aaf0c67
Nigel Mafukidze <ntmafukidze@gmail.com>
2021-11-24 17:32:17 +0200

    Use environment variables cfor database connection string

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm  import sessionmaker
+from .config import settings
 
-# BAD PRACTICE HARD CODING CREDENTIALS IN CODE
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Mahachula123#!@localhost/fastapi'
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+# import time
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine =  create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -17,3 +21,17 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# DATABASE CONNECTION FOR RAW SQL AND NOT SQLALCHEMY
+# while True:
+#     try:
+#         conn=psycopg2.connect(host="localhost",database="fastapi",user="postgres",
+#         password="Mahachula123#!",cursor_factory=RealDictCursor)
+#         cursor=conn.cursor()
+#         print("Database connected succesfully.")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed.")
+#         print("Error",error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,49 +1,28 @@
-from fastapi import FastAPI,Response, status, HTTPException,Depends
-from pydantic import BaseModel
-from random import randrange
-from typing import Optional, List
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models,schemas,utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post,user,auth
-
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
 app =FastAPI()
-
-# DATABASE CONNECTION
-while True:
-    try:
-        conn=psycopg2.connect(host="localhost",database="fastapi",user="postgres",
-        password="Mahachula123#!",cursor_factory=RealDictCursor)
-        cursor=conn.cursor()
-        print("Database connected succesfully.")
-        break
-    except Exception as error:
-        print("Connecting to database failed.")
-        print("Error",error)
-        time.sleep(2)
-
-# FIND ARRAY INDEX OF A SINGLE POST
-def find_index_post(id):
-    for index,post in enumerate(my_posts):
-        if post['id']==id:
-            return index
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
 #  HOME PAGE
-@app.get("/")
-async def root():
-    return {
-        "message":"Hello World."
-    }
+# @app.get("/")
+# async def root():
+#     return {
+#         "message":"Hello World."
+#     }
 
 
+
+# FIND ARRAY INDEX OF A SINGLE POST
+# def find_index_post(id):
+#     for index,post in enumerate(my_posts):
+#         if post['id']==id:
+#             return index
